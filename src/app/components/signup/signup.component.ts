090b1b35f6984722b25e04e03cd0ed4ba55f46f0
Paul Bora <paulbora_akcan@hotmail.com>
2024-10-22 10:02:10 +0200

    Form styling

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { FormsModule } from '@angular/forms';
+import { error } from 'node:console';
 
 @Component({
   selector: 'app-signup',
@@ -15,11 +16,15 @@ import { FormsModule } from '@angular/forms';
 export class SignupComponent {
   username = '';
   password = '';
+  errors:string[] = []
 
   constructor(private authService: AuthService, private router: Router) {}
 
   onSignup() {
-    console.log(this.username, this.password)
+    if(this.username.length < 4 || this.password.length < 4){
+      this.errors.push('Username and Password must be more than 3 characters')
+      return
+    }
     this.authService.signup(this.escapeHtml(this.username), this.escapeHtml(this.password)).subscribe({
       next: () => this.router.navigate(['/login']),
       error: (err) => console.error('Signup failed', err),
