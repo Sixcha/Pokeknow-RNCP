57c01a4c2b79882b7c28bcd47fb29aa0c70b2142
Paul Bora <paulbora_akcan@hotmail.com>
2025-01-20 02:36:25 +0100

    production version

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../../environments/environment.development';
+import { environment } from '../../environments/environment';
 
 export type PokemonType =
   'normal' | 'fire' | 'water' | 'electric' | 'grass' | 'ice' | 'fighting' | 'poison' |
